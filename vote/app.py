ba63bfd84411536743fef37236d5dee8fdac8cf0
Gourav Shah <gs@initcron.org>
2018-08-15 04:04:35 +0000

    - added multi stage dockerfile for worker app - added user config for vote app

--- vote/app.py
+++ vote/app.py
@@ -5,10 +5,10 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "Amazon Echo")
+option_b = os.getenv('OPTION_B', "Google Home")
 hostname = socket.gethostname()
-version = 'v1'
+version = 'v2'
 
 app = Flask(__name__)
 
